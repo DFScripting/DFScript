5ca961ffaa005ec1596c0595a260eddf9d1460fa
RedCommand <58099979+RedCommand-dev@users.noreply.github.com>
2022-08-21 14:12:00 +0300

    Fix ReceiveChatEvent
    Now returns a stripped version of the message instead of the stripped version + the colorcode one

--- src/main/java/io/github/techstreet/dfscript/mixin/game/MClientPlayNetworkHandler.java
+++ src/main/java/io/github/techstreet/dfscript/mixin/game/MClientPlayNetworkHandler.java
@@ -34,13 +34,13 @@ public class MClientPlayNetworkHandler {
             return;
         }
 
-        ReceiveChatEvent event = new ReceiveChatEvent(packet.content());
         EventManager.getInstance().dispatch(event);
         if (event.isCancelled()) {
             ci.cancel();
         }
 
         String packetString = packet.content().getString();
+        ReceiveChatEvent event = new ReceiveChatEvent(packetString);
 
         if (packetString.equals("» You are now in dev mode.")) {
             DevModeEvent modeEvent = new DevModeEvent();
