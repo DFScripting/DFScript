adfa8583b6c26a407a5a9139d490355a5849e4a5
GeorgeRNG <grogrng@gmail.com>
2022-11-29 19:15:02 +0000

    Redo ScriptDeletionScreen

--- src/main/java/io/github/techstreet/dfscript/screen/script/ScriptDeletionScreen.java
+++ src/main/java/io/github/techstreet/dfscript/screen/script/ScriptDeletionScreen.java
@@ -3,22 +3,24 @@ package io.github.techstreet.dfscript.screen.script;
 import io.github.techstreet.dfscript.DFScript;
 import io.github.techstreet.dfscript.screen.CScreen;
 import io.github.techstreet.dfscript.screen.widget.CButton;
+import io.github.techstreet.dfscript.screen.widget.CText;
 import io.github.techstreet.dfscript.script.Script;
 import io.github.techstreet.dfscript.script.ScriptManager;
+import net.minecraft.text.Text;
 
 public class ScriptDeletionScreen extends CScreen {
 
     public ScriptDeletionScreen(Script script) {
-        super(100,50);
+        super(103,27);
 
-        widgets.add(new CButton(5,4,90,20,"Delete",() -> {
+        widgets.add(new CText(5,5, Text.literal("Delete " + script.getName() + "?")));
+
+        widgets.add(new CButton(5,12,45, 10,"Delete",() -> {
             ScriptManager.getInstance().deleteScript(script);
             DFScript.MC.setScreen(new ScriptListScreen(true));
         }));
 
-        widgets.add(new CButton(5,28,90,20,"Abort",() -> {
-            DFScript.MC.setScreen(new ScriptListScreen(true));
-        }));
+        widgets.add(new CButton(52,12,45, 10,"Cancel",() -> DFScript.MC.setScreen(new ScriptListScreen(true))));
     }
 
     @Override
