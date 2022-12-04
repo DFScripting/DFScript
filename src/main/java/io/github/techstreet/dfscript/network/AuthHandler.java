a422cc80a02e4b85286b999076737e60e18d0e90
GeorgeRNG <grogrng@gmail.com>
2022-12-04 11:34:37 +0000

    Make authentication work

--- src/main/java/io/github/techstreet/dfscript/network/AuthHandler.java
+++ src/main/java/io/github/techstreet/dfscript/network/AuthHandler.java
@@ -1,9 +1,12 @@
 package io.github.techstreet.dfscript.network;
 
 import com.google.gson.JsonObject;
+import com.mojang.authlib.exceptions.AuthenticationException;
 import io.github.techstreet.dfscript.DFScript;
 import io.github.techstreet.dfscript.loader.Loadable;
+import io.github.techstreet.dfscript.network.request.ForbiddenException;
 import io.github.techstreet.dfscript.network.request.ServerCodeResponse;
+import net.minecraft.client.util.Session;
 import org.apache.commons.codec.digest.DigestUtils;
 
 import java.io.BufferedReader;
@@ -21,7 +24,6 @@ public class AuthHandler implements Loadable {
 
     @Override
     public void load() {
-
         try {
             regen();
         }
@@ -40,19 +42,21 @@ public class AuthHandler implements Loadable {
             e.printStackTrace();
             return;
         }
+        // STEP 2: Connect to the fake server, and get our code validated
         try {
             validateCode();
             this.valid = true;
-        } catch (Exception e) {
-            DFScript.LOGGER.error(e);
-            e.printStackTrace();
+        } catch (AuthenticationException | ForbiddenException e) {
             this.valid = false;
+            DFScript.LOGGER.error(e.getMessage());
+            e.printStackTrace();
         }
     }
 
     /**
      * Sends a code to the server, and generates a hash of this code and one returned by the server.
      * This code is to be used as the bearer token, it just has to be verified by connecting to the fake server.
+     * This also sends our username, which is used later for verification.
      * @return The common code hash
      * @throws IOException Any error in the networking
      */
@@ -70,6 +74,7 @@ public class AuthHandler implements Loadable {
 
         JsonObject jsonObject = new JsonObject();
         jsonObject.addProperty("client",clientCode);
+        jsonObject.addProperty("name",DFScript.MC.getSession().getUsername());
 
         try (OutputStream os = connection.getOutputStream()) {
             byte[] input = jsonObject.toString().getBytes(StandardCharsets.UTF_8);
@@ -84,11 +89,38 @@ public class AuthHandler implements Loadable {
                 response.append(responseLine.trim());
             }
             ServerCodeResponse servercodeResponse = DFScript.GSON.fromJson(response.toString(), ServerCodeResponse.class);
-            return DigestUtils.sha256Hex(servercodeResponse.getServerCode() + clientCode);
+            return DigestUtils.sha256Hex(clientCode + servercodeResponse.getServerCode());
         }
     }
 
-    private void validateCode() {
-        // TODO: validate code.
+    /**
+     * Joins a fake minecraft server, and notifies the backend.
+     * The server ID is the common hash, which is then verified as safe to use.
+     * If this completes, the common hash should be used as an authentication token.
+     * @throws AuthenticationException Likely logged out
+     * @throws IOException DFOnline Backend error
+     */
+    private void validateCode() throws AuthenticationException, IOException, ForbiddenException {
+
+        Session session = DFScript.MC.getSession();
+        DFScript.MC.getSessionService().joinServer(session.getProfile(), session.getAccessToken(), commonSecret.substring(0,30));
+
+        URL url = new URL(DFScript.BACKEND + "/user/verify/" + commonSecret);
+        DFScript.LOGGER.info(url.toString());
+        HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+        connection.setRequestMethod("GET");
+        connection.setRequestProperty("Accept", "application/json");
+        connection.setReadTimeout(500000000);
+        connection.setConnectTimeout(500000000);
+
+        int status = connection.getResponseCode();
+
+        if(status == 403) {
+            throw new ForbiddenException("Player is banned by the backend!");
+        }
+        if(status != 204) {
+            throw new IOException("Unexpected response code from backend: " + status);
+        }
+        // It worked.
     }
 }
---
+++ src/main/java/io/github/techstreet/dfscript/network/request/ForbiddenException.java
@@ -0,0 +1,7 @@
+package io.github.techstreet.dfscript.network.request;
+
+public class ForbiddenException extends Exception {
+    public ForbiddenException(String message) {
+        super(message);
+    }
+}
