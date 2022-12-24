dbac749ea08d713fd0fcaec13fea207a641c71f2
GeorgeRNG <grogrng@gmail.com>
2022-12-24 17:08:06 +0000

    Improve ScriptCreationScreen

--- src/main/java/io/github/techstreet/dfscript/screen/script/ScriptCreationScreen.java
+++ src/main/java/io/github/techstreet/dfscript/screen/script/ScriptCreationScreen.java
@@ -3,8 +3,11 @@ package io.github.techstreet.dfscript.screen.script;
 import io.github.techstreet.dfscript.DFScript;
 import io.github.techstreet.dfscript.screen.CScreen;
 import io.github.techstreet.dfscript.screen.widget.CButton;
+import io.github.techstreet.dfscript.screen.widget.CText;
 import io.github.techstreet.dfscript.screen.widget.CTextField;
 import io.github.techstreet.dfscript.script.ScriptManager;
+import net.minecraft.text.Text;
+
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -12,32 +15,31 @@ public class ScriptCreationScreen extends CScreen {
 
     // invalid file name chars
     // there's definitely a better place to put this but this felt the simplest rn
-    Pattern ILLEGAL_CHARS = Pattern.compile("[\\\\/:*?\"<>|]");
+    Pattern ILLEGAL_CHARS = Pattern.compile("[\\x00-\\x1f<>:\"\\\\/|?*]");
 
     protected ScriptCreationScreen() {
-        super(105, 60);
+        super(103,35);
 
-        CTextField name = new CTextField("My Script", 2, 2, 96, 36, true);
-
-        name.setChangedListener(() -> name.textColor = 0xFFFFFF);
-
-        widgets.add(name);
-
-        widgets.add(new CButton(2, 42, 48, 15, "Create", () -> {
-            String scriptName = name.getText();
-
-            Matcher m = ILLEGAL_CHARS.matcher(scriptName);
-
-            if (m.find()) {
-                name.textColor = 0xFF3333;
-                return;
-            }
-
+        widgets.add(new CText(4, 3, Text.literal("Name your script:")));
+        CTextField name = new CTextField("My Script", 3, 8, 95, 8, true);
+        CButton addButton = new CButton(3, 19, 46, 10, "Create", () -> {
+            Matcher m = ILLEGAL_CHARS.matcher(name.getText());
+            if(m.find()) return;
             ScriptManager.getInstance().createScript(name.getText());
             DFScript.MC.setScreen(new ScriptListScreen(true));
-        }));
-
-        widgets.add(new CButton(50, 42, 48, 15, "Cancel", () -> {
+        });
+        name.setChangedListener(() -> {
+            name.textColor = 0xFFFFFF;
+            addButton.setDisabled(false);
+            Matcher m = ILLEGAL_CHARS.matcher(name.getText());
+            if(m.find()) {
+                name.textColor = 0xFF3333;
+                addButton.setDisabled(true);
+            }
+        });
+        widgets.add(name);
+        widgets.add(addButton);
+        widgets.add(new CButton(52, 19, 46, 10, "Cancel", () -> {
             DFScript.MC.setScreen(new ScriptAddScreen());
         }));
     }
