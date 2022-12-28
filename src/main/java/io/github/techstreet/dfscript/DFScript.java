c06724466543f9d6e0a247b42eb38b47a7398177
GeorgeRNG <grogrng@gmail.com>
2022-12-28 19:58:06 +0000

    Warning(x, 1) [UNUSED_IMPORT] Unused import statement

--- src/main/java/io/github/techstreet/dfscript/DFScript.java
+++ src/main/java/io/github/techstreet/dfscript/DFScript.java
@@ -9,39 +9,18 @@ import io.github.techstreet.dfscript.features.AuthHandler;
 import io.github.techstreet.dfscript.features.UpdateAlerts;
 import io.github.techstreet.dfscript.loader.Loader;
 import io.github.techstreet.dfscript.screen.overlay.OverlayManager;
-import io.github.techstreet.dfscript.screen.script.ScriptListScreen;
 import io.github.techstreet.dfscript.script.ScriptManager;
-import io.github.techstreet.dfscript.script.util.AuthcodeResponse;
-import io.github.techstreet.dfscript.script.util.ServercodeResponse;
-import io.github.techstreet.dfscript.script.util.UploadResponse;
 import io.github.techstreet.dfscript.util.Scheduler;
-
-import java.io.BufferedReader;
-import java.io.ByteArrayOutputStream;
-import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.net.HttpURLConnection;
-import java.net.URL;
-import java.nio.file.Files;
-import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
-import java.util.zip.GZIPOutputStream;
-
-import io.github.techstreet.dfscript.util.chat.ChatType;
-import io.github.techstreet.dfscript.util.chat.ChatUtil;
 import net.fabricmc.api.ModInitializer;
 import net.fabricmc.fabric.api.command.v2.ArgumentTypeRegistry;
 import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
-import net.minecraft.client.util.Session;
-import net.minecraft.command.argument.serialize.ConstantArgumentSerializer;
-import net.minecraft.command.argument.serialize.StringArgumentSerializer;
 import net.minecraft.util.Identifier;
-import org.apache.commons.codec.binary.Base64;
-import org.apache.commons.lang3.RandomStringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.apache.logging.log4j.core.jmx.Server;
+
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class DFScript implements ModInitializer {
 
